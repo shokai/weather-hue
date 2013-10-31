0666bc00fcc96e088bc942ef2293ade0f8dafca7
Sho Hashimoto <hashimoto@shokai.org>
2013-10-31 17:40:33 +0900

    modify code

--- weather-hue.rb
+++ weather-hue.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-$stdout.sync = true
 require 'rubygems'
 require 'bundler'
 Bundler.require
@@ -9,32 +8,25 @@ args = ArgsParser.parse ARGV do
   arg :city, 'city', :alias => :c, :default => '東京'
   arg :rain, '降水確率のしきい値 (%)', :default => 30
   arg :help, 'show help', :alias => :h
+
+  on :help do
+    STDERR.puts help
+    STDERR.puts "e.g.  ruby #{$0} -city 鎌倉"
+    exit 1
+  end
 end
 
-if args.has_option? :help
-  STDERR.puts args.help
-  STDERR.puts "e.g.  ruby #{$0} -city 鎌倉"
-  exit 1
-end
-
-begin
-  weather = WeatherJp.get args[:city], :today
-rescue StandardError, Timeout::Error => e
-  STDERR.puts e
-  exit 1
-end
+weather = WeatherJp.get args[:city], :today
 puts "#{weather} - #{Time.now}"
 
-hue = Hue::Client.new
-light = hue.lights[0]
+light = Hue::Client.new.lights[0]
 puts "light : #{light.name}"
+light.on = true
+light.saturation = 180
+light.brightness = 200
 
 if weather.rain > args[:rain]
-  light.hue = 7000
-  light.saturation = 180
-  light.brightness = 200
+  light.hue = 60000
 else
-  light.hue = 0
-  light.saturation = 180
-  light.brightness = 200
+  light.hue = 46920
 end
